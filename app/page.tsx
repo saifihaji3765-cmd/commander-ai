5b17e2beb3ffe30367b0416e615ee54cf2caf366
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-01-18 13:43:10 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>COMMANDER AI</h1>
-      <p>Business ka dard samajhne wala AI</p>
-    </main>
-  );
-}
